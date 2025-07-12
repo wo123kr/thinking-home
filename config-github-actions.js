be8076e3cae35e6f71aafe1a35b52503d14941c3
mac-jinwoo <jinwoo@thinkingdata.kr>
2025-07-12 19:01:35 +0900

    chore: 테스트 파일 정리 (test-te-connection.js 삭제)

--- config-github-actions.js
+++ config-github-actions.js
@@ -12,7 +12,7 @@ const config = {
   // ThinkingData SDK 설정 (GitHub Actions용)
   thinkingData: {
     appId: getEnvVar('TE_APP_ID', '79ed7051fc51493798b16328c0ebd0bc'),
-    serverUrl: getEnvVar('TE_SERVER_URL', 'https://te-receiver-naver.thinkingdata.kr/sync_js'),
+    serverUrl: getEnvVar('TE_SERVER_URL', 'https://te-receiver-naver.thinkingdata.kr/sync_json'),
     showLog: false,
     autoTrack: {
       pageShow: true,
