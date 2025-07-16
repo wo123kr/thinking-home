0467b724336f46a4d363d373122b12e9e5474586
mac-jinwoo <jinwoo@thinkingdata.kr>
2025-07-16 16:09:58 +0900

    chore: 디버그 설정 비활성화 (운영환경 준비)

--- config.js
+++ config.js
@@ -96,9 +96,9 @@ const config = {
 
   // 디버그 설정
   debug: {
-    enabled: true, // 운영환경에서는 false, 개발환경에서는 true
+    enabled: false, // 운영환경에서는 false, 개발환경에서는 true
     logLevel: 'warn', // 'error', 'warn', 'info', 'debug'
-    showConsoleLogs: true // 우리가 만든 console.log들 제어 (테스트용)
+    showConsoleLogs: false // 우리가 만든 console.log들 제어 (테스트용)
   }
 };
 
